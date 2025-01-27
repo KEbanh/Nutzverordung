7856d82729536a58b5e3f34de03fec8e689a4fce
KEbanh <46811679+KEbanh@users.noreply.github.com>
2025-01-27 11:09:19 +0100

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,35 +1,43 @@
 <?php
+// Überprüfen, ob das Formular abgesendet wurde
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Formulardaten erhalten
-    $name = htmlspecialchars($_POST['name']);
-    $class = htmlspecialchars($_POST['class']);
-    $email = htmlspecialchars($_POST['email']);
-    $schoolYear = htmlspecialchars($_POST['schoolYear']);
     
-    // Inhalt der E-Mail
-    $subject = "Einverständniserklärung - Leo-Statz-Berufskolleg";
-    $message = "
-    Einverständniserklärung des Schülers/der Schülerin:
+    // Die Formulardaten erhalten
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $klasse = isset($_POST['klasse']) ? $_POST['klasse'] : '';
+    $schuljahr = isset($_POST['schuljahr']) ? $_POST['schuljahr'] : '';
     
-    Name: $name
-    Klasse: $class
-    E-Mail: $email
-    Schuljahr: $schoolYear
+    // Hausordnung und EDV-Nutzungsverordnung prüfen
+    $hausordnung = isset($_POST['hausordnung']) ? 'Ja' : 'Nein';
+    $edv_nutzungsverordnung = isset($_POST['edv_nutzungsverordnung']) ? 'Ja' : 'Nein';
     
-    Ich habe die Hausordnung und die EDV-Nutzungsverordnung des Leo-Statz-Berufskollegs gelesen und verstanden.
-    ";
-    
-    // Header für die E-Mail (z.B. Absender, Reply-To)
-    $headers = "From: no-reply@leo-statz-berufskolleg.de\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-    
-    // E-Mail an den angegebenen Empfänger senden
-    $recipient = "ebanhesaten@leo-statz-berufskolleg.de";
-    if (mail($recipient, $subject, $message, $headers)) {
-        echo "Die Einverständniserklärung wurde erfolgreich gesendet.";
+    // E-Mail-Einstellungen
+    $to = "eban@gmx.net";  // Empfänger der E-Mail
+    $subject = "Anmeldung von der Klasse: $klasse";  // Betreff mit der Klasse
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8" . "\r\n";
+    $headers .= "From: $email" . "\r\n";  // Absender-E-Mail-Adresse
+
+    // Nachricht im HTML-Format
+    $message = "<html><body>";
+    $message .= "<h2>Anmeldeformular</h2>";
+    $message .= "<p><strong>Name:</strong> $name</p>";
+    $message .= "<p><strong>E-Mail:</strong> $email</p>";
+    $message .= "<p><strong>Klasse:</strong> $klasse</p>";
+    $message .= "<p><strong>Schuljahr:</strong> $schuljahr</p>";
+    $message .= "<p><strong>Hausordnung gelesen:</strong> $hausordnung</p>";
+    $message .= "<p><strong>EDV-Nutzungsverordnung gelesen:</strong> $edv_nutzungsverordnung</p>";
+    $message .= "</body></html>";
+
+    // E-Mail senden
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Die E-Mail wurde erfolgreich gesendet!";
     } else {
-        echo "Fehler beim Senden der E-Mail. Bitte versuchen Sie es später erneut.";
+        echo "Es gab ein Problem beim Senden der E-Mail.";
     }
+} else {
+    // Falls das Formular nicht mit POST übermittelt wurde
+    echo "Fehler: Formular wurde nicht korrekt übermittelt.";
 }
 ?>
